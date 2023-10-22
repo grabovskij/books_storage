01077f2d5b180378f4f4b8ff5ed4da41cd9fb0f6
grabovskij.l <leonidgrabovskij941@gmail.com>
2023-10-22 20:01:39 +0300

    added library page

--- lib/domain/models/book_info.dart
+++ lib/domain/models/book_info.dart
@@ -4,6 +4,7 @@ class BookInfo {
   final String author;
 
   ///  Только положительные занчения
+  final int? id;
   final int year;
   final int pageCount;
 
@@ -13,5 +14,29 @@ class BookInfo {
     this.author = '',
     this.year = -1,
     this.pageCount = 0,
+    this.id,
   });
+
+  BookInfo copyWith({
+    int? id,
+    String? title,
+    String? publisher,
+    String? author,
+    int? year,
+    int? pageCount,
+  }) {
+    return BookInfo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      publisher: publisher ?? this.publisher,
+      author: author ?? this.author,
+      year: year ?? this.year,
+      pageCount: pageCount ?? this.pageCount,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'BookInfo(id: $id, title: $title, publisher: $publisher, author: $author, year: $year, pageCount: $pageCount)';
+  }
 }
